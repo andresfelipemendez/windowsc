7d41aaf1e54b516be1ab8d3a6fa87c2288ce6116
Andrés Felipe <fmendez1212@gmail.com>
2020-03-08 12:30:35 -0500

    window!

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-clang -I"C:\Program Files (x86)\Windows Kits\10\Include" -L"C:\Program Files (x86)\Windows Kits\10\Lib" -lUser32 -Wno-missing-declarations -Wno-invalid-token-paste -Wno-unused-value -x c++ test.c 
+clang -I"C:\Program Files (x86)\Windows Kits\10\Include" -L"C:\Program Files (x86)\Windows Kits\10\Lib" -lUser32 -lGdi32 -Wno-missing-declarations -Wno-invalid-token-paste -Wno-unused-value -x c++ test.c 
--- test.c
+++ test.c
@@ -1,12 +1,128 @@
-#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-int CALLBACK WinMain(
-    HINSTANCE hInstance,
-    HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine,
-    int nCmdShow
-) {
-    MessageBoxA(0, "This is winc", "win c", MB_OK|MB_ICONINFORMATION);
+LRESULT CALLBACK
+MainWindowCallback(HWND Window,
+                   UINT Message,
+                   WPARAM WParam,
+                   LPARAM LParam)
+{
+    LRESULT Result = 0;
+    switch (Message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE");
+        }
+        break;
+
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY");
+            PostQuitMessage(0);
+        }
+        break;
+
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE");
+        }
+        break;
+
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP");
+        }
+        break;
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+
+            static DWORD Operation = WHITENESS;
+
+            PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+            if(Operation == WHITENESS) {
+                Operation = BLACKNESS;
+            } else  {
+                Operation = WHITENESS;
+            }
+
+            EndPaint(Window, &Paint);
+            OutputDebugStringA("WM_ACTIVATEAPP");
+        }
+        break;
+
+        default:
+        {
+            //    OutputDebugStringA("default");
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        }
+        break;
+    }
+    return (Result);
+}
+
+int CALLBACK WinMain(HINSTANCE Instance,
+                     HINSTANCE PrevInstance,
+                     LPSTR CommandLine,
+                     int ShowCode)
+{
+    WNDCLASS wnd;
+    wnd.cbClsExtra = 0;
+    wnd.cbWndExtra = 0;
+    wnd.hCursor = LoadCursor(0, IDC_ARROW);
+    wnd.hIcon = LoadIcon(0, IDI_WINLOGO);
+    wnd.lpszMenuName = 0;
+    wnd.style = 0;
+    wnd.hbrBackground = 0;
+    wnd.lpfnWndProc = MainWindowCallback;
+    wnd.hInstance = Instance;
+    wnd.lpszClassName = "Window Class";
+
+    RegisterClass(&wnd);
+
+    HWND WindowHandle = CreateWindowEx(
+        0,
+        "Window Class",
+        "Windows Programming",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        Instance,
+        NULL);
+
+    if (WindowHandle == NULL)
+    {
+        //Pause
+        system("PAUSE");
+        return -1;
+    }
+
+    ShowWindow(WindowHandle, ShowCode);
+
+    MSG Message;
+    for (;;)
+    {
+        BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+        if (MessageResult > 0)
+        {
+            TranslateMessage(&Message);
+            DispatchMessage(&Message);
+        }
+        else
+        {
+            break;
+        }
+    }
+
     return 0;
 }
